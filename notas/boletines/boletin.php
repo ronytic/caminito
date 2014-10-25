3147fc209ad3f839af43fca3da6e4be544a92318
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-10-25 00:19:52 -0400

    Boletin

--- notas/boletines/boletin.php
+++ notas/boletines/boletin.php
@@ -3,11 +3,16 @@ include_once("../../login/check.php");
 if(!empty($_POST)){
 	$CodAlumno= $_POST['CodAlumno'];
 	$CodCurso=$_POST['CodCurso'];
+	
+	$archivo="verboletin.php?CodAlumno=".$CodAlumno."&CodCurso=".$CodCurso."&mf=".md5("lock")."&Bimestre=1";
 	?>
-   
-    <a href="../../impresion/notas/boletin.php?CodAlumno=<?php echo $CodAlumno;?>&CodCurso=<?php echo $CodCurso;?>&mf=<?php echo md5("lock")?>" class="boton corner-all">Boletin en grande</a> <a href="#" class="botonSec" id="registrarimpresion">Registrar Impresión</a>
-    <hr />
-    <iframe width="450" height="600" src="../../impresion/notas/boletin.php?CodAlumno=<?php echo $CodAlumno;?>&CodCurso=<?php echo $CodCurso;?>&mf=<?php echo md5("lock")?>"></iframe>
+   	<a href="<?php echo $archivo?>&Bimestre=1" class="botonSec" target="flotante">1º de Bimestre</a>
+    <a href="<?php echo $archivo?>&Bimestre=2" class="botonSec" target="flotante">2º de Bimestre</a>
+    <a href="<?php echo $archivo?>&Bimestre=3" class="botonSec" target="flotante">3º de Bimestre</a>
+    <hr>
+    <!--<a href="">4º de Bimestre</a>-->
+
+    <iframe width="450" height="600" src="<?php echo $archivo?>" name="flotante"></iframe>
     <div id="respuestaimpresiones"></div>
     <?php
 }
