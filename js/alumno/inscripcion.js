d8eaa726097119e46f3d95b2e615e26756490f14
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-01-18 18:06:58 -0400

    Inscripción

--- js/alumno/inscripcion.js
+++ js/alumno/inscripcion.js
@@ -4,16 +4,22 @@ $(document).ready(function(e) {
 	var MontoBeca;
    $('#FechaNac').datepicker({altField: "#FechaNac", maxDate:"0 D",changeMonth: true,changeYear: true,yearRange:"c-100:c+10",dateFormat: 'dd-mm-yy'});
    $('#FechaRetiro').datepicker({altField: "#FechaRetiro",changeMonth: true,changeYear: true,dateFormat: 'dd-mm-yy'})
+   sacarMonto();
 	$("#curso").change(function(e) {
-		var valor=$(this).val();
+		sacarMonto();
+    });
+	
+	function sacarMonto(){
+		var valor=$("#curso").val();
         $.post("../sacarmonto.php",{'CodCurso':valor},function(data){
 			$('#MontoPagar').val(data)
 			MontoGeneral=parseFloat(data);
 			MontoBeca=$("#MontoBeca").val();
-			$('#MontoPagar').val(MontoGeneral-($("#MontoBeca").val()));
-			$("#PorcentajeBeca").val((MontoBeca*100/MontoGeneral).toFixed(2));	
-		});
-    });
+			$('#MontoPagar').val(MontoGeneral-(MontoBeca));
+			PorcentajeBeca=isNaN((MontoBeca*100/MontoGeneral))?0:(MontoBeca*100/MontoGeneral);
+			$("#PorcentajeBeca").val(PorcentajeBeca.toFixed(2));	
+		});	
+	}
 	
 	if($('#MontoPagar').val()!="0"){
 		MontoGeneral=$('#MontoPagar').val();
